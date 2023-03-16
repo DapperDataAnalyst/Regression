7d928f64251f81918299f6a447e9d290c8862cd2
DapperDataAnalyst <112096821+DapperDataAnalyst@users.noreply.github.com>
2023-03-16 09:27:00 -0500

    Add files via upload

--- Regression Homework 7.R
+++ Regression Homework 7.R
@@ -5,7 +5,7 @@ y = unlist(data[1])
 x = unlist(data[2])
 n = nrow(data)
 
-t = 5
+t = 1
 
 for (k in 2:1000)
 {
@@ -14,11 +14,8 @@ for (k in 2:1000)
   t = t - u/d
 }
 
-y_hat = exp(t*x)
-e_hat = y - y_hat
 
-
-# Problem 1.3
+# Problem 1.3 & 1.4
 ttt = 0
 ttt_test = 0
 
@@ -28,17 +25,25 @@ for(kk in 1:100)
   qq = sample(q,replace=TRUE)
   xx = x[qq]
   yy = y[qq]
-  sigma_sqr = (1/(n-1)) * sum((yy-exp(t*x))^2)
-  yyy = exp(t*x) + sqrt(sigma_sqr)*rnorm(n)
+  sigma_hat = sqrt((1/(n-1)) * sum((y-exp(t*x))^2))
+  #sigma_hat = sqrt(var((yy-exp(t*x))))
+  yyy = exp(t*x) + sigma_hat*rnorm(n)
   
   ttt[1] = 1
-  for (k in 2:3000)
+  for (k in 2:300)
   {
     uu = sum(-x*yyy*exp(x*ttt[k-1])+x*exp(2*ttt[k-1]*x))
     dd = sum(-x^2*yyy*exp(x*ttt[k-1]) + (2*x^2)*exp(2*ttt[k-1]*x))
     ttt[k] = ttt[k-1] - uu/dd
   }
   
-  ttt_test[kk] = ttt[3000]
+  ttt_test[kk] = ttt[300]
 }
 
+# Problem 1.5
+alpha = 0.05
+x_bar = mean(x)
+log_y_hat = ttt_test*x_bar
+crit_val = 1.96
+lower_bound = mean(log_y_hat) - crit_val*sqrt(var(log_y_hat))
+upper_bound = mean(log_y_hat) + crit_val*sqrt(var(log_y_hat))
